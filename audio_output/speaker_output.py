b04397a5c0ec3c0ac324f774840737157109a568
Logan <lowei4805@gmail.com>
2025-10-15 15:46:01 +0200

    config-Datei für veränderliche Sound-Namen hinzugefügt

--- audio_output/speaker_output.py
+++ audio_output/speaker_output.py
@@ -1,14 +1,19 @@
 import rclpy
 from rclpy.node import Node
+import yaml
+import os
 
 from std_msgs.msg import Bool
 
 from playsound import playsound
 
+
 class SpeakerOutput(Node):
 
 	def __init__(self):
 		super().__init__('speaker_output')
+		with open("/home/logan/ros2_ws/src/audio_output/temp_config/soundpaths.yaml", "r") as config:
+			self.sounds = yaml.safe_load(config)
 		self.subscription = self.create_subscription(
 			Bool,
 			'pressed_key',
@@ -17,11 +22,14 @@ class SpeakerOutput(Node):
 		self.subscription
 
 	def audio_callback(self, msg):
+		sound1 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name1"])
+		sound2 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name2"])
+		
 		if msg.data:
-			playsound("/home/logan/ros2_ws/src/audio_output/audio_output/audio_files/test_sound_1.mp3")
+			playsound(sound1)
 			self.get_logger().info('Playing sound 1')
 		else:
-			playsound("/home/logan/ros2_ws/src/audio_output/audio_output/audio_files/test_sound_2.mp3")
+			playsound(sound2)
 			self.get_logger().info('Playing sound 2')
 
 def main(args=None):
