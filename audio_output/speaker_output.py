142eda45a42bbfaf9dac7c904105d32cd40f7c87
Logan <lowei4805@gmail.com>
2025-10-19 13:03:30 +0200

    Intergration von audioplayer/simpleaudio, um asynchrones abspielen zu ermöglichen

--- audio_output/speaker_output.py
+++ audio_output/speaker_output.py
@@ -5,7 +5,9 @@ import os
 
 from sensor_msgs.msg import Joy
 
-from playsound import playsound
+#from playsound import playsound
+#from audioplayer import AudioPlayer
+import simpleaudio
 
 class SpeakerOutput(Node):
 
@@ -15,43 +17,42 @@ class SpeakerOutput(Node):
 			self.sounds = yaml.safe_load(config)
 		self.subscription = self.create_subscription(
 			Joy,
-			'/joy',				#TODO: topic-Name ändern
+			'/joy',
 			self.audio_callback,
 			10)
 		self.subscription
 
 	def audio_callback(self, msg):
-		sound1 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name1"])
-		sound2 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name2"])
-		sound3 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name3"])
-		sound4 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name4"])
+		soundpath1 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name1"])
+		soundpath2 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name2"])
+		soundpath3 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name3"])
+		soundpath4 = os.path.join(self.sounds["soundfolder"], self.sounds["filenames"]["name4"])
 
 		#Indizes für Nintendo-Controller, je nach Controller abändern
 		dpad_horizontal = msg.axes[4]		#1.0 == links, -1.0 == rechts
 		dpad_vertical 	= msg.axes[5]		#1.0 == oben, -1.0 == unten
 		
 		if dpad_vertical == 1.0 and dpad_horizontal == 0.0:
-			playsound(sound1)
+			#playsound(soundpath1)
+			#AudioPlayer(soundpath1).play(block=True)
 			self.get_logger().info('Playing sound 1')
 		elif dpad_horizontal == -1.0 and dpad_vertical == 0.0:
-			playsound(sound2)
+			#playsound(soundpath2)
+			#AudioPlayer(soundpath2).play(block=True)
 			self.get_logger().info('Playing sound 2')
 		elif dpad_vertical == -1.0 and dpad_horizontal == 0.0:
-			playsound(sound3)
+			#playsound(soundpath3)
+			#AudioPlayer(soundpath3).play(block=True)
+			waveobject3 = simpleaudio.WaveObject.from_wave_file(soundpath3)
+			sound3 = waveobject3.play()
+			sound3.wait_done()
 			self.get_logger().info('Playing sound 3')
 		elif dpad_horizontal == 1.0 and dpad_vertical == 0.0:
-			playsound(sound4)
+			#playsound(soundpath4)
+			#AudioPlayer(soundpath4).play(block=True)
 			self.get_logger().info('Playing sound 4')
 		else:
 			self.get_logger().info('Not playing a sound')
-		
-
-
-		#if msg.data:
-		#	playsound(sound3)
-		#	self.get_logger().info('Playing sound 3')
-		#else:
-		#	self.get_logger().info('not playing a sound')
 
 def main(args=None):
 	rclpy.init(args=args)
